beaa78ac3c94451a3de19625e2a9459d2f22b106
Warrenww <warrenpig@gmail.com>
2019-04-02 15:14:30 +0800

    index

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
 
 });
+
+['newPage','newEvent'].map((x)=>{
+  var temp = xmlHttp(`./public/data/${x}.txt`);
+  console.log(temp);
+})
+function xmlHttp (url) {
+  if (!url) return null;
+  else return new Promise(function(resolve, reject) {
+    var request = new XMLHttpRequest();
+    request.open("GET",url, true);
+    request.send();
+    request.onreadystatechange = function () {
+      if(this.readyState == 4 && this.status == 200) resolve(this.responseText);
+      else reject(this);
+    }
+  });
+}
