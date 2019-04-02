b7bbc333b28f8f5719e4686af9d4bae318159d5a
Warrenww <warrenpig@gmail.com>
2019-04-02 15:37:10 +0800

    fetch test

--- public/js/index.js
+++ public/js/index.js
@@ -3,18 +3,13 @@ $(document).ready(function () {
 });
 
 ['newPage','newEvent'].map((x)=>{
-  var temp = xmlHttp(`./public/data/${x}.txt`);
-  console.log(temp);
-})
-function xmlHttp (url) {
-  if (!url) return null;
-  else return new Promise(function(resolve, reject) {
-    var request = new XMLHttpRequest();
-    request.open("GET",url, true);
-    request.send();
-    request.onreadystatechange = function () {
-      if(this.readyState == 4 && this.status == 200) resolve(this.responseText);
-      else reject(this);
-    }
-  });
-}
+  fetch(`./public/data/${x}.txt`).then(response => {
+    response.text().then(data => {
+      data = data.split("\n");
+      data.map(tr => {
+        tr = tr.split(",");
+        $("#"+x).append(`<tr><th>${tr[0]}</th><td><a href='${tr[2]}'>${tr[1]}</a></td></tr>`);
+      });
+    });
+  }).catch(e=>{console.log(e);})
+});
